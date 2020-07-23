318e1c056f6f7bec02e8d4db259722f79366fca9
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-23 16:45:18 -0500

    DTO implement

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body } from '@nestjs/common';
 
+import { CreateProductDTO } from './dto/product.dto'
+
 @Controller('product')
 export class ProductController {
 
   @Post('/create')
-  createPost(@Res() res, @Body() createProductDTO) {
+  createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
     return res.status(HttpStatus.OK).json({
       message: 'received'
     })
