7070715e6d47cf3ace2898a0380cf822f4765220
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-23 23:58:49 -0500

    lot of changes [connect db - methods crud]

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ProductModule],
+  imports: [
+    ProductModule, 
+    MongooseModule.forRoot('mongodb+srv://db_user_platzivideos:rompiendola.desde.l9l9@cluster0-f0pdk.mongodb.net/alexa?retryWrites=true&w=majority')
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,14 +1,27 @@
 import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body } from '@nestjs/common';
 
 import { CreateProductDTO } from './dto/product.dto'
+import { ProductService } from './product.service'
 
 @Controller('product')
 export class ProductController {
 
+  constructor(private productService: ProductService) {}
+
   @Post('/create')
-  createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
+  async createPost(@Res() res, @Body() createProductDTO: CreateProductDTO) {
+    const product = await this.productService.createProduct(createProductDTO)
     return res.status(HttpStatus.OK).json({
-      message: 'received'
+      message: 'Product Successfully Created',
+      product
+    })
+  }
+
+  @Get('/')
+  async getProducts(@Res() res) {
+    const products = await this.productService.getProducts()
+    return res.status(HttpStatus.OK).json({
+      products
     })
   }
 
