268f4da6cfa90d01df27d3acc6ccea6a7ec3a75c
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-24 00:16:50 -0500

    product deleted

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body, Param, NotFoundException } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body, Param, NotFoundException, Query } from '@nestjs/common';
 
 import { CreateProductDTO } from './dto/product.dto'
 import { ProductService } from './product.service'
@@ -30,6 +30,16 @@ export class ProductController {
     const product = await this.productService.getProduct(productID)
     if (!product) throw new NotFoundException('Product Does not exists')
     return res.status(HttpStatus.OK).json(product)
+  }
+
+  @Delete('/delete')
+  async deleteProduct(@Res() res, @Query('productID') productID) {
+    const productDeleted = await this.productService.deletedProduct(productID)
+    if (!productDeleted) throw new NotFoundException('Product Does not exists')
+    return res.status(HttpStatus.OK).json({
+      message: 'Product deleted successfully',
+      productDeleted
+    })
   }
 
 }
