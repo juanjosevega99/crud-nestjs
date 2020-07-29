1fe035d3e195d2cc16d5df41a1a033be8db06ee6
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-29 09:10:00 -0500

    db name replaced

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 @Module({
   imports: [
     ProductModule, 
-    MongooseModule.forRoot('mongodb+srv://db_user_platzivideos:rompiendola.desde.l9l9@cluster0-f0pdk.mongodb.net/alexa?retryWrites=true&w=majority')
+    MongooseModule.forRoot('mongodb+srv://db_user_platzivideos:rompiendola.desde.l9l9@cluster0-f0pdk.mongodb.net/test?retryWrites=true&w=majority')
   ],
   controllers: [AppController],
   providers: [AppService],
