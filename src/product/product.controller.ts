d595af46c41787591fa6083565816861d79add71
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-07-29 08:42:56 -0500

    [ADD] no-eslint to a file

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import { Controller, Get, Post, Put, Delete, Res, HttpStatus, Body, Param, NotFoundException, Query } from '@nestjs/common';
 
 import { CreateProductDTO } from './dto/product.dto'
