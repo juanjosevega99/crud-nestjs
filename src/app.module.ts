ddac8b8b38da8a234242761ecaabff165dcac6dd
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-08-21 23:28:37 -0500

    [FIX] import and inect service and DB

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
-import { MongooseModule } from '@nestjs/mongoose';
 
 import { TypegooseModule } from 'nestjs-typegoose'
 import configuration from './config/config';
@@ -10,19 +9,20 @@ import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
   imports: [
-    ProductModule, 
     ConfigModule.forRoot({
       load: [configuration],
     }),
     TypegooseModule.forRootAsync({
       imports:[ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get('mongoURI'),
+        uri: configService.get('databaseMongodb'),
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true,
       }),
-    })
+      inject: [ConfigService],
+    }),
+    ProductModule, 
   ],
   controllers: [AppController],
   providers: [AppService],
